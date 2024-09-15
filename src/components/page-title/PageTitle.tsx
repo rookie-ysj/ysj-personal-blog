3d3f198675cd415cbf93837b6c583713676e1471
YangSJ <445501072@qq.com>
2024-09-15 21:52:42 +0800

    add husky

--- src/components/page-title/PageTitle.tsx
+++ src/components/page-title/PageTitle.tsx
@@ -3,9 +3,15 @@ import ChangeTheme from '../change-theme/ChangeTheme.tsx'
 import './index.scss'
 
 export default function PageTitle() {
+  const a = 1
   return (
     <header className="page-title bg-background">
-      <img src="/blog.svg" alt="logo" className="logo" />
+      <img
+        src="/blog.svg"
+        alt="logo"
+        className="logo cursor-pointer"
+        onClick={() => window.location.replace(window.location.origin)}
+      />
       <div className="flex">
         <ChangeTheme />
         <a href="https://github.com/rookie-ysj" target="_blank">
