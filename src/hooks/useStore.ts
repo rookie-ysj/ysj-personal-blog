0a4f652638826ecac15fc1d9e2f4f2fd71eb28b4
rookie-ysj <ysj15148242832@gmail.com>
2024-11-18 10:56:12 +0800

    clear

--- src/hooks/useStore.ts
+++
@@ -1,6 +0,0 @@
-import type { TypedUseSelectorHook } from 'react-redux'
-import type { Dispatch, RootState } from '../store'
-import { useDispatch, useSelector } from 'react-redux'
-
-export const useCustomDispatch: () => Dispatch = useDispatch
-export const useCustomSelector: TypedUseSelectorHook<RootState> = useSelector
--- src/types/article.d.ts
+++
@@ -1,8 +0,0 @@
-declare interface Article {
-  id: string
-  title: string
-  description: string
-  content: string
-  type: string
-  createTime: string
-}
